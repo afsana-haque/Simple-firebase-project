9fbfcdd8ca1f0ee1d3072cab589044ae8bb023c1
afsana-haque <afsana.haque3301@gmail.com>
2023-04-25 03:09:10 +0600

    added register code

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SocialLoginBtn from '../SocialLoginBtn/SocialLoginBtn';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../../firebase/firebase.config';
+import Button from 'react-bootstrap/Button';
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [control, setControl] = useState(false);
+
+  const auth = getAuth(app);
+
+   const handleRegister = (event)=>{
+    event.preventDefault();
+
+    if (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password)) {
+      console.log("okay tikh ace ");
+    } else {
+      setError("password tikh nai");
+      return;
+    }
+
+    if(email && password){
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+      setError("");
+    }
+   else {
+    setError("broo hobe na email and password lagbe");
+  }
+   }
+   console.log(email, password)
+
     return (
         <div>
                   <div className="container">
         <div className="row d-flex justify-content-center align-items-center">
           <div className="col-md-6 ">
             <div className="border w-100 m-auto text-center p-5">
-              <p className="text-danger"></p>
+            <p className="text-danger">{error}</p>
               <form action="">
   
                 <input
@@ -18,23 +56,34 @@ const Register = () => {
                   required
                 />
                 <input
+                onChange={(e) => setEmail(e.target.value)}
                   className="email p-3 m-2"
                   type="email"
                   placeholder="enter your email"
                 />
                 <div className="pass-container">
-                <input
+                  {control ? (
+                    <input
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="password p-3 m-2"
+                      type="text"
+                      value={password}
+                      placeholder="enter your password"
+                      
+                    /> 
+                  ) : (
+                    <input
+                      onChange={(e) => setPassword(e.target.value)}
                       className="password p-3 m-2"
                       type="password"
                       placeholder="enter your password"
                     />
+                  )}
+                   <Button onClick={() => setControl(!control)}>toggle</Button>
                 </div>
-                <button
-                 
-                  className="btn btn-info w-75 p-2 mt-3"
-                >
-                  Register
-                </button>
+               
+                <button onClick={handleRegister} className="btn btn-info w-75 p-2 mt-3">
+                  Register</button>
                 <p className="p-2">
                   <small className="text-info">
                     already have account? login here..
